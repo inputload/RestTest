dc275b1f86d4cb364ac426c689de03ecbf1b5232
Mikhail Pavlov <mm4.pavlov@yandex.ru>
2022-05-31 20:30:24 +0300

    init

--- rest_test/main_app/urls.py
+++ rest_test/main_app/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', MainView.as_view()),  # Путь который отобразит MainView
     path('sum/', SumView.as_view()),  # +
+    path('cur/', BankView.as_view()),  # +
 ]
--- rest_test/main_app/views.py
+++ rest_test/main_app/views.py
@@ -1,3 +1,5 @@
+import time
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -14,8 +16,35 @@ class SumView(APIView):
         # До "1,15,7,12"
         numbers = numbers.split(',')  # Разделили строку на разные строки по запятой
         # После ["1","15","7","12"]
-        print(numbers)
-        return Response({'res': ''})
+
+        # Первый вариант
+
+        # res = 0
+        # for num in numbers:
+        #     res += float(num)
+
+        # Второй вариант
+
+        res = sum([float(num) for num in numbers])
+
+        return Response({'res': res})
 
     def post(self, request):
         pass
+
+
+class BankView(APIView):
+    def get(self, request):
+        currency = {
+            "BYN": 2.6,
+            "EUR": 1.1,
+            "PLN": 4.5,
+        }
+        cur = request.GET.get("cur")
+        count = request.GET.get("count")
+        return Response({
+            "cur": cur,
+            f"USD to {cur}": f"{currency[cur]}",
+            "res": f"{currency[cur]*float(count)}",
+            "time": f"{time.ctime()}"
+        })
